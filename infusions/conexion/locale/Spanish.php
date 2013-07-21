ed7a8ac4ea37931fc426cb39251670f5edbc3521
SpamFusion <Spam@ZoneIRC.Net>
2013-07-21 13:16:50 -0500

    Create Spanish.php

---
+++ infusions/conexion/locale/Spanish.php
@@ -0,0 +1,27 @@
+<?php
+$locale['df_conexion'] = "Iniciar Sesión";
+$locale['df_conexion_desc'] = "Sistema De Conexión";
+
+$locale['df_conexion_avatar'] = "Invitado [Avatar]";
+
+$locale['df_conexion_mensaje'] = "Mensajes Privados";
+$locale['df_conexion_msg'] = "Tienes %u ";
+$locale['df_conexion_msg1'] = "Mensaje";
+$locale['df_conexion_msg2'] = "Mensajes";
+
+$locale['df_conexion_m1'] = " Abrir Panel ";
+$locale['df_conexion_m2'] = " Cerrar Panel ";
+
+$locale['df_conexion_i1'] = " Abrir Información ";
+$locale['df_conexion_i2'] = " Cerrar Información ";
+
+$locale['df_conexion_login'] = "Iniciar Sesión";
+
+$locale['df_conexion_invitado'] = "Bienvenido Invitado";
+
+$locale['df_conexion_st'] = "Información";
+$locale['df_conexion_ip'] = "IP";
+$locale['df_conexion_id'] = "ID";
+$locale['df_conexion_foro'] = "Mensajes Del Foro";
+
+?>
