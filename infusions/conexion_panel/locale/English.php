9ee77088eb634ba7fc3623a9afe026747338b281
SpamFusion <Spam@ZoneIRC.Net>
2013-07-21 13:30:56 -0500

    Create English.php

---
+++ infusions/conexion_panel/locale/English.php
@@ -0,0 +1,27 @@
+<?php
+$locale['df_conexion'] = "Login";
+$locale['df_conexion_desc'] = "System Connection";
+
+$locale['df_conexion_avatar'] = "Guest [Avatar]";
+
+$locale['df_conexion_mensaje'] = "Private Messages";
+$locale['df_conexion_msg'] = "You Have %u ";
+$locale['df_conexion_msg1'] = "Message";
+$locale['df_conexion_msg2'] = "Messages";
+
+$locale['df_conexion_m1'] = " Open Panel ";
+$locale['df_conexion_m2'] = " Close Panel ";
+
+$locale['df_conexion_i1'] = " Open Info ";
+$locale['df_conexion_i2'] = " Close Info ";
+
+$locale['df_conexion_login'] = "Login";
+
+$locale['df_conexion_invitado'] = "Welcome Guest";
+
+$locale['df_conexion_st'] = "Information";
+$locale['df_conexion_ip'] = "IP";
+$locale['df_conexion_id'] = "ID";
+$locale['df_conexion_foro'] = "Forum Posts";
+
+?>
